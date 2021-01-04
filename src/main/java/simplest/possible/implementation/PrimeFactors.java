9a9096df5e2b9e8d276e7bae4e9d782bca34a4de
sowmya-AS <76809502+sowmya-AS@users.noreply.github.com>
2021-01-04 13:32:05 +0530

    Add files via upload

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,14 @@
+package simplest.possible.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+
+    public <T> List<T> of(int i) {
+
+        ArrayList primeFactors=new ArrayList();
+
+        return primeFactors;
+    }
+}
